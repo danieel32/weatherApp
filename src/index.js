998a108a96af8932d0777a73eee945b44b92dbae
danieel32 <110873554+danieel32@users.noreply.github.com>
2023-01-07 10:15:06 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const sunrise = new Date((data.sys.sunrise + data.timezone) * 1000)
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
