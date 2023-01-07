0916aead22324cb7e6add2c88110ca4a480c3101
danieel32 <110873554+danieel32@users.noreply.github.com>
2023-01-07 13:05:22 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -35,17 +35,6 @@ function App() {
           <div className="temp">
             {data.main ?<h1>{data.main.temp.toFixed()}F</h1>: null}
           </div>
-          <div className="time">
-            <div id="icon">
-              <div>
-                 <img 
-                    id="wicon" 
-                    src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
-                    alt="weather icon">
-                 </img>
-              </div>
-            </div>
-          </div>
         </div>
         {data.name != undefined &&
         <div className='bottom'>
